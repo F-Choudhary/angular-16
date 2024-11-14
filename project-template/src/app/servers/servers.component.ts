502d3f8e70122f10fc6d55a7f1af7599b48322c6
Fakhruddin Choudhary <72145757+F-Choudhary@users.noreply.github.com>
2024-11-14 13:18:21 +0000

    second commit

---
+++ project-template/src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers', // as tag
+  // selector: '[app-servers]',  //as attribute 
+  // selector: '.app-servers',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
