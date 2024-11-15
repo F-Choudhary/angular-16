7e1d174e41a0c93bd45f1121a06fd8a48affdcf1
Fakhruddin Choudhary <72145757+F-Choudhary@users.noreply.github.com>
2024-11-15 12:50:47 +0000

    databinding

--- 1-basic-angular/src/app/servers/servers.component.ts
+++ 1-basic-angular/src/app/servers/servers.component.ts
@@ -8,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './servers.component.css'
 })
 export class ServersComponent {
+  allowNewServer = false
+  serverCreationStatus = "No server was created"
 
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer=true;
+    },2000)
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = "Server was created"
+  }
+
+  onUpdateServerName(){
+    
+  }
 }
